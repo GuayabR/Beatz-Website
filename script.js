8e2f66167d0da9a59e277a5fef284b3ca82ca001
GuayabR <160454807+GuayabR@users.noreply.github.com>
2025-09-02 22:23:53 +1200

    Fix download again

--- script.js
+++ script.js
@@ -38,25 +38,3 @@ function detectDeviceType() {
 function home() {
     window.location.href = "/";
 }
-
-function download() {
-    // Replace with your GitHub repo, release tag, and asset name
-    const githubUrl = "https://github.com/GuayabR/Beatz-X/releases/download/v1.0/BeatzX-1.0.zip";
-
-    fetch(githubUrl)
-        .then((response) => {
-            if (!response.ok) throw new Error("Network response was not ok");
-            return response.blob();
-        })
-        .then((blob) => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.href = url;
-            a.download = "Beatz! X 1.0.0.zip"; // downloaded file name
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-            window.URL.revokeObjectURL(url);
-        })
-        .catch((err) => console.error("Download failed:", err));
-}
